6083819c2d344bab29a6c6c0978438bccf911249
Daniel Primo <delineas@gmail.com>
2019-12-01 22:12:28 +0100

    🚧 Add CORS

--- bootstrap/web.php
+++ bootstrap/web.php
@@ -3,6 +3,19 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
+
+$app->options('/{routes:.+}', function ($request, $response, $args) {
+    return $response;
+});
+
+$app->add(function ($request, $handler) {
+    $response = $handler->handle($request);
+    return $response
+        ->withHeader('Access-Control-Allow-Origin', '*')
+        ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+});
+
 $app->get('/test', function (Request $request, Response $response, $args) {
     $response->getBody()->write($this->get('hello'));
     return $response;
